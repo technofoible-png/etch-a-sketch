dd1bbb3ed2f652a7ff76d8bc1bf5bc973b05c377
technofoible <techno.foible@gmail.com>
2021-05-04 14:35:57 -0500

    added button to clear 'drawing' from grid

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const main = document.querySelector(".main");
+const reset = document.querySelector(".reset");
 
 for (let i=0; i < 16; i++) {
-    let div = document.createElement("div");
-    div.classList.add("pad");
-    div.textContent =  i;
-    main.appendChild(div);
+    const pad = document.createElement("div");
+    pad.classList.add("pad");
+    pad.addEventListener("mouseover", drawOn);
+    main.appendChild(pad);
 }
+
+function drawOn(event) {
+    event.target.classList.add("marked");
+}
+
+reset.addEventListener("click", clearBoard);
+
+function clearBoard () {
+    let mmmm = document.querySelectorAll(".marked");
+    mmmm.forEach(mmm => mmm.classList.remove("marked"));
+}
+
