0b0160c6f769f0861dc18679fe5739f7da552897
technofoible <techno.foible@gmail.com>
2021-05-04 14:49:27 -0500

    grid size now customizable

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 const main = document.querySelector(".main");
 const reset = document.querySelector(".reset");
 
-for (let i=0; i < 16; i++) {
-    const pad = document.createElement("div");
-    pad.classList.add("pad");
-    pad.addEventListener("mouseover", drawOn);
-    main.appendChild(pad);
-}
 
+
+// Colors in grid
 function drawOn(event) {
     event.target.classList.add("marked");
 }
 
+// Reset button handler
 reset.addEventListener("click", clearBoard);
 
+// Erases the grid
 function clearBoard () {
-    let mmmm = document.querySelectorAll(".marked");
-    mmmm.forEach(mmm => mmm.classList.remove("marked"));
+    let gridItems = document.querySelectorAll(".marked");
+    gridItems.forEach(gridItem => gridItem.classList.remove("marked"));
 }
 
+// Creates a grid with specified number rows & cols
+function makeGrid (size) {
+    let gridArea = size * size;
+    for (let i=0; i < gridArea; i++) {
+        const pad = document.createElement("div");
+        pad.classList.add("pad");
+        main.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+        main.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+        pad.addEventListener("mouseover", drawOn);
+        main.appendChild(pad);
+    }
+}
